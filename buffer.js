ff1131fb7d3fcf13510112a4dc3898d66ca90fa0
CS 341 Infrastructure <cs341admin@cs.illinois.edu>
2026-02-08 21:35:13 -0600

    Add files via upload

---
+++ buffer.js
@@ -0,0 +1,13 @@
+const specs = {
+    "shelln": "Make sure to write the #n shell utility in a standalone function with signature shell_341_05(int n, ...). You can use variadic arguments inorder to customize this function how you need but make sure n is the first argument and the function shell_341_05 is present for the autograder.",
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Find all spans with the 'dynamic-spec' class
+    document.querySelectorAll('.spec').forEach(el => {
+        const specId = el.getAttribute('data-spec-id');
+        if (specs[specId]) {
+            el.innerHTML = specs[specId];
+        }
+    });
+});
