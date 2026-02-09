68485b485ffc7c376b7f35355f95bd282d9b2f2c
CS 341 Infrastructure <cs341admin@cs.illinois.edu>
2026-02-08 22:58:47 -0600

    Update buffer.js

--- buffer.js
+++ buffer.js
@@ -1,5 +1,6 @@
 const specs = {
     "shelln": "Make sure to write the #n shell utility in a standalone function with signature shell_341_05(int n, ...). You can use variadic arguments inorder to customize this function how you need but make sure n is the first argument and the function shell_341_05 is present for the autograder.",
+    "shellor": "For readability we recommend implementing the or function separately in a function named shell_or_341_01(). This will help staff review your code.",
 };
 
 document.addEventListener("DOMContentLoaded", () => {
