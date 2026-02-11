51f2ecc45036e43cd2c1423aae0a571f42532b22
CS 341 Infrastructure <cs341admin@cs.illinois.edu>
2026-02-10 23:04:30 -0600

    Update buffer.js

--- buffer.js
+++ buffer.js
@@ -2,6 +2,9 @@ const specs = {
     "shelln": "Make sure to write the #n shell utility in a standalone function with signature shell_341_05(int n, ...). You can use variadic arguments inorder to customize this function how you need but make sure n is the first argument and the function shell_341_05 is present for the autograder.",
     "shellor": "For readability we recommend implementing the or function separately in a function named shell_or_341_01(). This will help staff review your code.",
     "shellappend": "On exit for debugging save the history to file .tmp_history in a function named debug_exit_tmp(). This is needed for the autograder to test your exit functionality.",
+
+    "mmglobal": "Make sure to initialize these variables in a function called mm_globals_alloc. This function should first set the values all to 341 and then to their desired value.",
+    "mmextra": "For proof of understanding and prep for the Shell MP part 2, create a function called mm_vspace_proc() that uses /proc fs to check the amount of virtual memory the current process uses. You don't have to call it but we will check it in the autograder.",
 };
 
 document.addEventListener("DOMContentLoaded", () => {
